c9d1cc736d596490f47990ab6fdfacbe0cacd237
jbrakensiek <jbrakensiek@users.noreply.github.com>
2014-05-16 18:46:36 -0700

    Improvements to command-line tool.

--- base/corbits.cpp
+++ base/corbits.cpp
@@ -7,11 +7,31 @@
 bool report_error = false;
 bool compute_all = false;
 const char* DEFAULT_FORMAT = "nPeioOu";
-const char* short_options = "ef:o:A";
+const char* short_options = "ef:ho:A";
 
 static struct option long_options[] = {
   {"help", no_argument, 0, 0}
 };
+
+void set_input_orbit_default (input_orbit &io) {
+  io.a = 1;
+  io.e = 0;
+  io.i = 0;
+  io.omega = 0;
+  io.Omega = 0;
+  io.P = 365.25;
+  io.r = 0;
+  io.r_star = 1;
+  io.use = 0;
+}
+
+void fix_input_orbit (input_orbit &io) {
+  io.i *= DEG_TO_RAD;
+  io.omega *= DEG_TO_RAD;
+  io.Omega *= DEG_TO_RAD;
+  io.r_star *= SR_TO_AU;
+  io.use ^= 1;
+}
 
 bool read(FILE* fin, int &len, char* format, input_orbit io[]) {
   /* for (int i = 0; i < 20; i++) {
@@ -38,6 +58,8 @@ bool read(FILE* fin, int &len, char* format, input_orbit io[]) {
       token[0] = line;
       int cur = 1;
       int line_len = strlen(line);
+      bool did_a = false;
+      bool did_P = false;
       for (int i = 0; i < line_len; i++) {
 	if (line[i] == '\t') {
 	  token[cur++] = line + i + 1;
@@ -48,11 +70,13 @@ bool read(FILE* fin, int &len, char* format, input_orbit io[]) {
 	fprintf (stderr, "Error: Incorrect number of tokens in row %d\n", len + 1);
 	return false;
       }
+      set_input_orbit_default(io[len]);
       for (int i = 0; i < format_len; i++) {
 	int a;
 	// fprintf (stderr, "CUR: %c\n", format[i]);
 	switch (format[i]) {
 	case 'a':
+	  did_a = true;
 	  a = sscanf (token[i], "%lf", &io[len].a);
 	  break;
 	case 'e':
@@ -72,6 +96,7 @@ bool read(FILE* fin, int &len, char* format, input_orbit io[]) {
 	  a = sscanf (token[i], "%lf", &io[len].Omega);
 	  break;
 	case 'P':
+	  did_P = true;
 	  a = sscanf (token[i], "%lf", &io[len].P);
 	  break;
 	case 'r':
@@ -92,8 +117,53 @@ bool read(FILE* fin, int &len, char* format, input_orbit io[]) {
 	  return false;
 	}
       }
+      fix_input_orbit(io[len]);
+      if (did_a && !did_P) {
+	io[len].P = sqrt (pow(io[len].a, 3) / mass_from_radius(io[len].r_star)) * DAYS_IN_YEAR;
+      }
+      else if (!did_a && did_P) {
+	io[len].a = radius(mass_from_radius(io[len].r_star), io[len].P);
+      }
     }
   }
+}
+
+void print_usage(char *prog) {
+  printf ("Usage: %s [-h|--help] [-f FORMAT] [-e] [-A] < FILENAME\n\n", prog);
+}
+
+void print_help(char *prog) {
+  print_usage(prog);
+  printf ("Compute the geometric probability of observing\n");
+  printf ("the transit of all exoplanets specified in FILENAME\n");
+  printf ("from the perspective of a random observer.\n\n");
+  printf ("Options:\n");
+  printf ("  -h --help    prints help information then exits\n");
+  printf ("  -f FORMAT    specifies format of input (see below)\n");
+  printf ("  -e           include worst-case error bounds in output\n");
+  printf ("  -A           compute probabilities for all possible observations\n");
+  printf ("\n");
+  printf ("The input, read from stdin or FILENAME, should be a tab-delimited\n");
+  printf("table, each row of which specific an individual planet.  The orbital\n");
+  printf ("parameters represented by each column should be specific to each format.\n");
+  printf ("If certain orbital parameters are omitted, their values will be set to\n");
+  printf ("a given default value or be inferred from the other specific values.\n");
+  printf ("The possible formats, specified by the string FORMAT, and their default\nvalues if not selected are:\n");
+  printf ("  a            the semi-major axis in AU (default: 1)\n");
+  printf ("  e            the eccentricity (default: 0)\n");
+  printf ("  i            the inclination in degrees (default: 0)\n");
+  printf ("  n            the name of the planet (default: NULL)\n");
+  printf ("  o            the argument of periapsis in degrees (default: 0)\n");
+  printf ("  O            the longitude of the ascending node in degrees (default: 0)\n");
+  printf ("  P            the period of the orbit in days (default: 365.25)\n");
+  printf ("  r            the radius of the planet in earth radii (default: 0)\n");
+  printf ("  R            the radius of the star in stellar radii (default: 1)\n");
+  printf ("  u            0 explicitly includes the planet, 1 explicitly excludes the  \n               planet from the observation (default: 0)\n");
+  printf ("\n");
+  printf ("CORBITS will have an exit status of  0 if successful. If there are\nerrors in the reading of the table, an exit status of 1 will be returned.\nAny other errors will return an exit status of 2.\n");
+  printf ("\n");
+  printf ("For more information about CORBITS:\n");
+  printf ("<https://www.github.com/jbrakensiek/CORBITS/>\n");
 }
 
 int main(int argc, char **argv) {
@@ -105,6 +175,7 @@ int main(int argc, char **argv) {
     int c, option_index = 0;
     c = getopt_long(argc, argv, short_options,
 		    long_options, &option_index);
+
     if (c == -1)
       break;
     
@@ -112,51 +183,25 @@ int main(int argc, char **argv) {
     case 0:
       // fprintf (stderr, "Option: %s\n", long_options[option_index].name);
       if (option_index == 0) {
-	printf ("Usage: %s [--help] [-e] [-f FORMAT] < FILENAME\n\n", argv[0]);
-        printf ("Compute the geometric probability of observing\n");
-	printf ("the transit of all exoplanets specified in FILENAME\n");
-	printf ("from the perspective of a random observer.\n\n");
-	printf ("Options:\n");
-	printf ("  --help       prints help information then exits\n");
-	printf ("  -f FORMAT    specifies format of input (see below)\n");
-	printf ("  -e           include worst-case error bounds in output\n");
-	printf ("  -A           compute probabilities for all possible observations\n");
-	printf ("\n");
-	printf ("The input, read from stdin or FILENAME, should be a tab-delimited\n");
-	printf("table, each row of which specific an individual planet.  The orbital\n");
-	printf ("parameters represented by each column should be specific to each format.\n");
-	printf ("If certain orbital parameters are omitted, their values will be set to\n");
-	printf ("a given default value or be inferred from the other specific values.\n");
-	printf ("The possible formats, specified by the string FORMAT, and their default\nvalues if not selected are:\n");
-	printf ("  a            the semi-major axis in AU (default: 1)\n");
-	printf ("  e            the eccentricity (default: 0)\n");
-	printf ("  i            the inclination in degrees (default: 0)\n");
-	printf ("  n            the name of the planet (default: NULL)\n");
-	printf ("  o            the argument of periapsis in degrees (default: 0)\n");
-	printf ("  O            the longitude of the ascending node in degrees (default: 0)\n");
-	printf ("  P            the period of the orbit in days (default: 365.25)\n");
-	printf ("  r            the radius of the planet in earth radii (default: 0)\n");
-	printf ("  R            the radius of the star in stellar radii (default: 1)\n");
-	printf ("  u            0 explicitly includes the planet, 1 explicitly excludes the  \n               planet from the observation (default: 0)\n");
-        printf ("\n");
-	printf ("CORBITS will have an exit status of  0 if successful. If there are\nerrors in the reading of the table, an exit status of 1 will be returned.\nAny other errors will return an exit status of 2.\n");
-	printf ("\n");
-	printf ("For more information about CORBITS:\n");
-	printf ("<https://www.github.com/jbrakensiek/CORBITS/>\n");
+	print_help(argv[0]);
 	return 0;
       }
       break;
     case 'e':
-      fprintf (stderr, "Option: Report Error\n");
+      // fprintf (stderr, "Option: Report Error\n");
+      report_error = true;
       break;
     case 'f':
-      fprintf (stderr, "Option: Format: %s\n", optarg);
+      // fprintf (stderr, "Option: Format: %s\n", optarg);
       if (strlen (optarg) > 20) {
 	fprintf (stderr, "Error: format string exceeds maximum length of 20 characters\n");
 	return 1;
       }
       strcpy (format, optarg);
       break;
+    case 'h':
+      print_help(argv[0]);
+      return 0;
     case 'o':
       fout = fopen (optarg, "r");
       break;
@@ -164,18 +209,20 @@ int main(int argc, char **argv) {
       compute_all = true;
       break;
     case '?':
-      break;
+      print_usage(argv[0]);
+      return 2;
     default:
       fprintf (stderr, "Error: getopt\n");
-      return 1;
+      print_usage(argv[0]);
+      return 2;
     }
   }
-  fprintf (stderr, "optind: %d\n", optind);
+  // fprintf (stderr, "optind: %d\n", optind);
   int len = 0;
   input_orbit io[MAX];
-  for (int i = 0; i < n; i++) {
+  /* for (int i = 0; i < n; i++) {
     
-  }
+     } */
 
   if (!read(fin, len, format, io)) {
     return 2;
@@ -193,15 +240,17 @@ int main(int argc, char **argv) {
   else {
     for (int i = 0; i < (1 << len); i++) {
       for (int j = 0; j < len; j++) {
-	io[j].use = ((1 << j) & (1 << i)) > 0;
-	sci_value prob = prob_of_transits_input_orbit(len, io);
-
-	fprintf (fout, "%s", use_string);
-	fprintf (fout, "Probability: %e\n", prob.val);
-	if (report_error) {
-	  fprintf (fout, "Positive Error: %e\n", prob.pos_err);
-	  fprintf (fout, "Negative Error: %e\n", prob.neg_err);
-	}
+	io[j].use = ((1 << j) & i) == 0;
+      }
+      for (int k = 0; k < len; k++) {
+	printf ("%d", io[k].use ^ 1);
+      }
+      fprintf (fout, "\n");
+      sci_value prob = prob_of_transits_input_orbit(len, io);
+      fprintf (fout, "Probability: %e\n", prob.val);
+      if (report_error) {
+	fprintf (fout, "Positive Error: %e\n", prob.pos_err);
+	fprintf (fout, "Negative Error: %e\n", prob.neg_err);
       }
     }
   }
