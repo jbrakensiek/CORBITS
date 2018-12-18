8bc28e1bd1ba4bc5924ccc0c8e4d9c31883125d7
Eric Ford <eford@psu.edu>
2018-12-18 15:15:59 -0500

    adding test

---
+++ test/runtests.jl
@@ -0,0 +1,16 @@
+using Base.Test
+
+# write your own tests here
+@test 1 == 1
+
+function test_using()
+  try 
+     using CORBITS
+  catch
+     return false
+  end
+  return true
+end
+
+@test test_using()
+
